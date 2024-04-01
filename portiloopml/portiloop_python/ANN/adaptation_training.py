Fixed issue with RMS calculation

portiloopml/portiloop_python/ANN/adaptation_training.py
@@ -19,7 +19,7 @@ from portiloopml.portiloop_python.ANN.data.mass_data_new import (
     MassConsecutiveSampler, MassDataset)
 from portiloopml.portiloop_python.ANN.utils import set_seeds
 from portiloopml.portiloop_python.ANN.validation_mass import load_model_mass
-from portiloopml.portiloop_python.ANN.wamsley_utils import (RMS_score, binary_f1_score,
+from portiloopml.portiloop_python.ANN.wamsley_utils import (RMS_score_all, binary_f1_score,
                                                             detect_lacourse,
                                                             get_spindle_onsets)
 
@@ -1049,12 +1049,10 @@ def spindle_metrics(labels, preds, data=None, ss_labels=None, threshold=0.5, sam
     }
     if data is not None:
         # Compute the average RMs score for all the spindles
-        rms_scores = []
-        for spindle in onsets_preds:
-            if spindle < 3750 or spindle > len(data) - 3750:
-                continue
-            spindle_data = torch.tensor(data[spindle-3750:spindle+3750])
-            rms_scores.append(RMS_score(spindle_data))
+        print(f"We have {len(onsets_preds)} spindles")
+        start = time.time()
+        rms_scores = RMS_score_all(data, onsets_preds)
+        print(f"RMS took {time.time() - start}")
         rms_scores = np.array(rms_scores)
         rms_scores = rms_scores[~np.isnan(rms_scores)]
         rms_score = np.mean(rms_scores)

portiloopml/portiloop_python/ANN/wamsley_utils.py
@@ -435,8 +435,37 @@ def smooth(data, dur, s_freq):
     return data
 
 
-def RMS_score(candidate, Fs=250, lowcut=11, highcut=16):
+# def RMS_score(candidate, Fs=250, lowcut=11, highcut=16):
 
+#     # Filter the signal
+#     stopbbanAtt = 60  # stopband attenuation of 60 dB.
+#     width = .5  # This sets the cutoff width in Hertz
+#     nyq = 0.5 * Fs
+#     ntaps, _ = kaiserord(stopbbanAtt, width/nyq)
+#     atten = kaiser_atten(ntaps, width/nyq)
+#     beta = kaiser_beta(atten)
+#     a = 1.0
+#     taps = firwin(ntaps, [lowcut, highcut], nyq=nyq,
+#                   pass_zero=False, window=('kaiser', beta), scale=False)
+#     filtered_signal = torch.tensor(filtfilt(taps, a, candidate).copy())
+    
+
+#     # Get the baseline and the detection window for the RMS
+#     detect_index = len(candidate) // 2
+#     size_window = int(0.5 * Fs)
+#     baseline_idx = -2 * Fs  # Index compared to the detection window
+#     baseline = filtered_signal[detect_index +
+#                                baseline_idx:detect_index + baseline_idx + size_window]
+#     detection = filtered_signal[detect_index:detect_index + size_window]
+
+#     # Calculate the RMS
+#     baseline_rms = torch.sqrt(torch.mean(torch.square(baseline)))
+#     detection_rms = torch.sqrt(torch.mean(torch.square(detection)))
+
+#     score = detection_rms / baseline_rms
+#     return score
+
+def filter_signal_for_RMS(signal, Fs=250, lowcut=11, highcut=16):
     # Filter the signal
     stopbbanAtt = 60  # stopband attenuation of 60 dB.
     width = .5  # This sets the cutoff width in Hertz
@@ -447,11 +476,12 @@ def RMS_score(candidate, Fs=250, lowcut=11, highcut=16):
     a = 1.0
     taps = firwin(ntaps, [lowcut, highcut], nyq=nyq,
                   pass_zero=False, window=('kaiser', beta), scale=False)
-    filtered_signal = torch.tensor(filtfilt(taps, a, candidate).copy())
-    
+    filtered_signal = torch.tensor(filtfilt(taps, a, signal).copy())
+    return filtered_signal
 
+def get_RMS_score(filtered_signal, index, Fs=250):
     # Get the baseline and the detection window for the RMS
-    detect_index = len(candidate) // 2
+    detect_index = index
     size_window = int(0.5 * Fs)
     baseline_idx = -2 * Fs  # Index compared to the detection window
     baseline = filtered_signal[detect_index +
@@ -464,3 +494,11 @@ def RMS_score(candidate, Fs=250, lowcut=11, highcut=16):
 
     score = detection_rms / baseline_rms
     return score
+
+def RMS_score_all(signal, indexes):
+    filtered_signal = filter_signal_for_RMS(signal)
+    scores = []
+    for index in indexes:
+        score = get_RMS_score(filtered_signal, index)
+        scores.append(score)
+    return scores